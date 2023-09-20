4f6285c62bc0b15d8a14965e1ab093ba21399ef8
Nathanael Vieira <vieira_trabalho@hotmail.com>
2023-09-20 14:45:21 -0300

    finalizado: deixando o sistema finalizado, futuramente irei mexer em algo mais.

--- src/application/MenuController.java
+++ src/application/MenuController.java
@@ -3,7 +3,9 @@ package application;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.animation.FadeTransition;
 import javafx.animation.ScaleTransition;
+import javafx.animation.TranslateTransition;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -25,17 +27,68 @@ public class MenuController implements Initializable {
 	private AnchorPane stageLate;
 
 	@FXML
-	ImageView exitImageView1;
+	private ImageView exitImageView1;
 
 	@FXML
-	ImageView exitImageView11;
+	private ImageView exitImageView11;
+
+	@FXML
+	private AnchorPane pane1, pane2;
+
+	@FXML
+	private ImageView menu;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 
+		pane1.setVisible(false);
+
+		FadeTransition fadeTransition = new FadeTransition(Duration.millis(500), pane1);
+		fadeTransition.setFromValue(1.0);
+		fadeTransition.setToValue(0);
+		fadeTransition.play();
+
+		TranslateTransition transition = new TranslateTransition(Duration.millis(500), pane2);
+		transition.setByX(-600);
+		transition.play();
+
+		menu.setOnMouseClicked(event -> {
+			pane1.setVisible(true);
+
+			if (pane2.getTranslateX() == -600) {
+				FadeTransition fadeTransition1 = new FadeTransition(Duration.millis(500), pane1);
+				fadeTransition.setFromValue(0);
+				fadeTransition.setToValue(0.15);
+				fadeTransition.play();
+
+				TranslateTransition transition1 = new TranslateTransition(Duration.millis(500), pane2);
+				transition.setByX(+600);
+				transition.play();
+			}
+
+		});
+
+		pane1.setOnMouseClicked(event -> {
+			FadeTransition fadeTransition2 = new FadeTransition(Duration.millis(500), pane1);
+			fadeTransition.setFromValue(0.15);
+			fadeTransition.setToValue(0);
+			fadeTransition.play();
+
+			fadeTransition.setOnFinished(events -> {
+				pane1.setVisible(false);
+			});
+
+			TranslateTransition transition1 = new TranslateTransition(Duration.millis(500), pane2);
+			transition.setByX(-600);
+			transition.play();
+		});
+
 		actionToLeave();
 		moveScreen();
+		styleInitial();
+	}
 
+	private void styleInitial() {
 		stageAnchorPane.setStyle("""
 				-fx-border-color: black;
 				-fx-border-width: 1.5px;
@@ -52,6 +105,13 @@ public class MenuController implements Initializable {
 				-fx-background-color: white;
 				""");
 
+		pane2.setStyle("""
+				-fx-border-color: black;
+				-fx-border-width: 1.5px;
+				-fx-border-style: solid;
+				-fx-border-radius: 3px;
+				-fx-background-color: white;
+				""");
 	}
 
 	private void moveScreen() {
